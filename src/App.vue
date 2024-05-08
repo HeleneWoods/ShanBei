0bb1e019c3f4a0908ba83dad6e851df48f08e625
Helene Woods <396293674@qq.com>
2024-05-08 15:46:47 +0800

    home page header

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,33 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import theHeader from './components/TheHeader.vue'
+import welcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="shanBei-layout">
+    <div class="container">
+      <div id="header">
+        <theHeader />
+      </div>
+      <div id="main">
+      </div>
+      <div id="footer">footer</div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss" scoped>
+.shanBei-layout {
+  width: 100vw;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+#header {
+  position: fixed;
+  top: 0;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  z-index: 999;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#main {
+  padding-top: 64px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+})
+</script>
+
+<template>
+	<div class="chef">
+		<div class="chef-main">
+			<img class="logo" src="https://media-image1.baydn.com/storage_media_image/bmpdqn/73cb83b320b2db49e20345369dcbab1f.c69dc8680bb430cca7205e078b234bda.jpg?x-oss-process=image/quality,q_80" title="logo" alt="logo">
+			<div class="nav-bar">
+				<!-- 锚点跳转同页面 -->
+				<a class="nav-anchor" href="">单词</a>
+				<a class="nav-anchor" href="">阅读</a>
+				<a class="nav-anchor" href="">听力口语</a>
+				<a class="nav-anchor" href="">编程</a>
+				<!-- 外链 -->
+				<a class="nav-anchor" href="https://company.shanbay.com/" target="_blank">关于公司</a>
+			</div>
+			<div class="login-btn-wrap">
+				<el-button class="login-btn shanBei-btn">登陆/注册</el-button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.chef {
+		width: 100%;
+		height: 64px;
+
+		.chef-main {
+			width: 940px;
+			margin: 0 auto;
+			display: flex;
+			align-items: center;
+
+			.nav-bar {
+				display: flex;
+				margin-left: 70px;
+			}
+		}
+	}
+	.logo {
+		width: 90px;
+		height: 56px;
+	}
+	.nav-anchor {
+		width: 96px;
+	}
+	a.nav-anchor {
+		text-decoration: none;
+		color: #333;
+	}
+	.login-btn-wrap {
+		margin-left: 114px;
+
+		.login-btn {
+			font-size: 12px;
+		}
+	}
+</style>
